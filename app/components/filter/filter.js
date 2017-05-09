cbe4cba6181ee20a3775632c0268f4ac04ad9c68
Avishay Pelach <avishay.pelach@gmail.com>
2017-05-09 16:33:14 +0300

    disign app

--- app/components/filter/filter.js
+++ app/components/filter/filter.js
@@ -17,7 +17,7 @@ export  default  class Filter extends React.Component {
 
 
   createGenresList() {
-    function reqListener (e) {
+    function reqListener(e) {
       const genreData = JSON.parse(e.target.responseText);
       console.info(genreData.genres);
 
@@ -25,7 +25,7 @@ export  default  class Filter extends React.Component {
       const genres = genreData.genres.map((genreObj) => {
         console.info(genreObj);
         return (
-          <label  key = { genreObj.id}> { genreObj.name }< input type = "checkbox" key = { genreObj.id} /></label>
+          <label key={ genreObj.id}>< input type="checkbox" key={ genreObj.id}/> { genreObj.name }</label>
         )
       });
 
@@ -39,41 +39,44 @@ export  default  class Filter extends React.Component {
     oReq.open("GET", "https://api.themoviedb.org/3/genre/movie/list?api_key=022c8dc6705c5ca51f38b984d6a5be4a&language=en-US");
     oReq.send();
   }
-  componentDidMount () {
+
+  componentDidMount() {
     this.createGenresList()
   }
 
   render() {
 
     return (
-      <div className="filter">
-        <h1>Filter....</h1>
+      <div className="filter" >
+        <h1 className="filter-header">Let’s flicks you some movies</h1>
 
-        <div className="genre">
-          <span className="genre-title">Genre</span>
-          <span className="ganres-selected">All</span>
-          <div className="lang-toggler-btn"> > </div>
-          <div className="genres-checkbox-list">{this.state.genres}</div>
-        </div>
+        <div className="filter-area">
+          <div className="genre">
+            <span className="genre-title">Genre</span>
+            <div className="genre-toggle-btn"> ></div>
+            <div className="genres-checkbox-list">{this.state.genres}</div>
+          </div>
 
-        <div className="lang">
-          <span className="lang-title">Language</span>
-          <span className="lang-selected">All</span>
-          <div> ></div>
-        </div>
+          <div className="lang">
+            <span className="lang-title">Language</span>
+            <div className="lang-toggle-btn"> ></div>
+            <div className="lang-checkbox-list"><label>< input type="checkbox" key=""/> english </label>
+            </div>
+          </div>
 
-        <div className="min-rate">
-          <MinMax />
-        </div>
+          <div className="min-rate right-color">
+            <MinMax/>
+          </div>
 
-        <div className="years-range">
-          <YearsRange />
-        </div>
+          <div className="years-range right-color">
+            <YearsRange />
+          </div>
 
-        <div className="string-filter">
-          <input type="text" placeholder="Find movie by name/actor/director"/>
+          <div className="string-filter">
+            <input type="text" placeholder="Or search by actor, actress, director, producer"/>
+          </div>
+          <div className="search-btn"><span> START NOW </span></div>
         </div>
-        <div className="search-btn"><span>Find me movies!</span></div>
       </div>
     )
   }
